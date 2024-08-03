3c1b482e7006d3cec7a26435a6a18ce187bdefde
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-03 22:09:58 +0500

    read and write files

---
+++ day3/fileSystem/index.js
@@ -0,0 +1,36 @@
+const {readFile, writeFile} = require('node:fs');
+const path = require('path');
+
+// it Buffer, and shows only ASCI code 
+// readFile("./intro.txt", (err,data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+// it shows Buffer, but we use data.toString() 
+// readFile("./intro.txt", (err,data) => {
+//     if(err) throw err;
+//     console.log(data.toString());
+// });
+
+// it shows now actual content, we use 'utf-8' after path;
+// readFile(path.join(__dirname, 'intro.txt'),'utf-8' ,(err,data) => {
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+
+// :: WRITE A FILE ::
+
+writeFile(path.join(__dirname,'files', 'message.txt'),'Writter by NodeJS.',(err) => {
+    if(err) throw err;
+    console.log('Done..');
+});
+
+// console.log('Lonewolf is hunting...');
+
+// exit on uncaught errors
+process.on('uncaughtException', (err) => {
+    console.log(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
