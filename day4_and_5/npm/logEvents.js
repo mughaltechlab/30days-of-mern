f272015521fc167ea35cff9a390b519899a3b986
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-06 15:11:34 +0500

    Update logEvents.js

--- day4_and_5/npm/logEvents.js
+++ day4_and_5/npm/logEvents.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const { format } = require('date-fns');
+const { v4 : uuid } = require('uuid');
+
+const path = require('path');
 
 console.log(format(new Date() , 'yyyy-MM-dd\tHH:mm:ss'));
