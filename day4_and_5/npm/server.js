126924ad3eb8fe044c39cac5522265d8b2e9f49c
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-08 00:18:25 +0500

    Update server.js

--- day4_and_5/npm/server.js
+++ day4_and_5/npm/server.js
@@ -16,6 +16,59 @@ const PORT = process.env.PORT || 3500;
 // create a minimal server
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    let filePath;
+
+    // if (req.url === '/' || req.url === 'index.html') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/html');
+    //     filePath = path.join(__dirname, 'views', 'index.html');
+    //     fs.readFile(filePath, 'utf8', (err, data) => {
+    //         res.end(data);
+    //     })
+    // }
+
+
+    // :: worst case :: lengthy and failure code ::
+
+    // switch (req.url) {
+    //     case '/':
+    //         res.statusCode = 200;
+    //         res.setHeader('Content-Type', 'text/html');
+    //         filePath = path.join(__dirname, 'views', 'index.html');
+    //         fs.readFile(filePath, 'utf8', (err, data) => {
+    //             res.end(data);
+    //         });
+            
+    //         break;
+    
+    // }
+
+
+    // switch (req.url) {
+    //     case 'classy.html':
+    //         res.statusCode = 200;
+    //         res.setHeader('Content-Type', 'text/html');
+    //         filePath = path.join(__dirname, 'views', 'index.html');
+    //         fs.readFile(filePath, 'utf8', (err, data) => {
+    //             res.end(data);
+    //         });
+            
+    //         break;
+    
+    //     default:
+    //         res.statusCode = 404;
+    //         res.setHeader('Content-Type', 'text/html');
+    //         filePath = path.join(__dirname, 'views', '404.html');
+    //         fs.readFile(filePath, 'utf8', (err, data) => {
+    //             res.end(data);
+    //         });
+    //         break;
+    // }
+
+    // :: worst case :: lengthy and failure code END ::
+
+    
 });
 
 server.listen(PORT,() => {
