6ed82eae98d6a0f3a21c3c8ed323ed51c919d2bb
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-08 14:53:04 +0500

    Update server.js

--- day4_and_5/npm/server.js
+++ day4_and_5/npm/server.js
@@ -17,7 +17,42 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
-    let filePath;
+    let extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+            break;
+    }
+
+    let filePath = 
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
 
     // if (req.url === '/' || req.url === 'index.html') {
     //     res.statusCode = 200;
