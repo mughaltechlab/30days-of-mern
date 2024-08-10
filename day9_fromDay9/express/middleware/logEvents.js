acf4fba2e406a2e52734b52c1928233ff5a1c502
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-08-10 23:52:53 +0500

    using middleware

--- day9_fromDay9/express/middleware/logEvents.js
+++ day9_fromDay9/express/middleware/logEvents.js
@@ -9,22 +9,29 @@ const path = require('path');
 
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyy-MM-dd\tHH:mm:ss')}`;
-    const check = fs.existsSync(path.join(__dirname, 'logs')) ? '\n' : '';
+    const check = fs.existsSync(path.join(__dirname, '..', 'logs')) ? '\n' : '';
     const logItem = `${check}${dateTime}\t${uuid()}\t${message}`;
     console.log(logItem);
     try {
-        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, 'logs'), err => {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'), err => {
                 if (err) throw err;
                 console.log('logs Directory created.')
             });
         }
 
-        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
 
     } catch (err) {
         console.log('logEvents function error : ' + err);
     }
 }
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+    const msg = `${req.method}\t${req.headers.origin}\t${req.url}`;
+    logEvents(msg,'reqLogs.txt');
+    console.log(req.headers.origin);
+    next();
+};
+
+module.exports = {logger, logEvents};
--- day9_fromDay9/express/server.js
+++ day9_fromDay9/express/server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {logger} = require('./middleware/logEvents.js')
+
+/**
+ * Middleware
+ * There are three types of Middleware, Built-in, custom, third-party middleware
+ */
+
+// custom middleware : we create a log function
+// app.use((req, res, next) => {
+//     const msg = `${req.method}\t${req.headers.origin}\t${req.url}`;
+//     logEvents(msg,'reqLogs.txt');
+//     console.log(req.headers.origin);
+//     next();
+// });
+// app.use((req,res,next) => logger(req,res,next));
+app.use(logger);
+
+/**
+ * built-in middleware to handle urlencoded data
+ * in other words, form data;
+ * 'content-type: application/x-www-form-urlencoded'
+ */
+app.use(express.urlencoded({extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+// server static files
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 // PORT
 const PORT = process.env.PORT || 3500;
@@ -10,14 +40,48 @@ app.get('^/$|/index(.html)?', (req, res) => {
     // res.sendFile('./views/index.html', {root: __dirname});
     // res.sendFile(path.join(__dirname, 'views', 'index.html'));
     
-    res.sendFile(path.join(__dirname, 'views', 'index.html'), (err) => {
-        if (err) console.log(err);
-        res.end();
-    })
-})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
 
 app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); // by default redirect status code 302 but we want 301,
+});
+
+// route handlers
+app.get('/wolf(.html)?', (req, res, next) => {
+    console.log('Lone Wolf is missing');
+    next();
+}, (req, res) => {
+    res.send('LONE WOLF is WATCHING YOU....');
+});
+
+
+const one = (req, res, next) => {
+    console.log('One');
+    next();
+};
+
+const two = (req, res, next) => {
+    console.log('Two');
+    next();
+};
+
+const three = (req, res) => {
+    console.log('Three');
+    res.send('Hunts Finished');
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
+// default becasue express routes check in waterfall
+app.get('/*', (req, res) => {
+    res
+        .status(404)
+        .sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
